7167da73dd602cc3b436dcfa0d1d27bc78e82d65
ProjectSoft <projectsoft2009@yandex.ru>
2025-10-20 17:50:44 +0400

    update

--- food-uploader-plugin.php
+++ food-uploader-plugin.php
@@ -15,7 +15,7 @@
 	Requires at least:  5.7
 	Requires PHP:       7.4
 	Creation Date:      2025-02-06 04:18:00
-	Last Update:        2025-10-20 17:46:36
+	Last Update:        2025-10-20 17:49:59
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
